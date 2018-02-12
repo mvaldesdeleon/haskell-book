8abe08a69c2e875be039d98290a48971e46e0b66
Martín Valdés de León <m.valdesdeleon@sovanta.com>
2018-02-12 20:46:36 +0100

    Finish Chapter 9.

--- ch09/stdfuns.hs
+++ ch09/stdfuns.hs
@@ -2,44 +2,64 @@ module StdFuns where
 
 -- myOr returns True if any Bool in the list is True
 myOr :: [Bool] -> Bool
-myOr = undefined
+myOr [] = False
+myOr (x:xs) = if x then True
+                   else myOr xs
 
 -- myAny returns True if a -> Bool applied to any of the values in the list returns True
 myAny :: (a -> Bool) -> [a] -> Bool
-myAny = undefined
+myAny _ [] = False
+myAny f (x:xs) = if f x then True
+                        else myAny f xs
 
 -- After you write the recursive myElem, write another version that uses any.
 myElem :: Eq a => a -> [a] -> Bool
-myElem = undefined
+myElem _ [] = False
+myElem y (x:xs) = if y == x then True
+                            else myElem y xs
+
+myElem' :: Eq a => a -> [a] -> Bool
+myElem' y = myAny $ (==) y
 
 -- Implement myReverse.
 myReverse :: [a] -> [a]
-myReverse = undefined
+myReverse [] = []
+myReverse (x:xs) = myReverse xs ++ [x]
 
 -- squish flattens a list of lists into a list
 squish :: [[a]] -> [a]
-squish = undefined
+squish [] = []
+squish (x:xs) = x ++ squish xs
 
 -- squishMap maps a function over a list and concatenates the results.
 squishMap :: (a -> [b]) -> [a] -> [b]
-squishMap = undefined
-
+squishMap f = squish . map f
 
 -- squishAgain flattens a list of lists into a list. This time re-use the squishMap function.
 squishAgain :: [[a]] -> [a]
-squishAgain = undefined
+squishAgain = squishMap id
 
 -- myMaximumBy takes a comparison function and a list and returns the greatest element of the list based on the last value that the comparison returned GT for.
 myMaximumBy :: (a -> a -> Ordering) -> [a] -> a
-myMaximumBy = undefined
+myMaximumBy f (x:[]) = x
+myMaximumBy f (x:xs) = case f x mxs of
+                         LT -> mxs
+                         EQ -> x
+                         GT -> x
+                       where mxs = myMaximumBy f xs
 
 -- myMinimumBy takes a comparison function and a list and returns the least element of the list based on the last value that the comparison returned LT for.
 myMinimumBy :: (a -> a -> Ordering) -> [a] -> a
-myMinimumBy = undefined
+myMinimumBy f (x:[]) = x
+myMinimumBy f (x:xs) = case f x mxs of
+                         LT -> x
+                         EQ -> x
+                         GT -> mxs
+                       where mxs = myMinimumBy f xs
 
 -- Using the myMinimumBy and myMaximumBy functions, write your own versions of maximum and minimum.
 myMaximum :: (Ord a) => [a] -> a
-myMaximum = undefined
+myMaximum = myMaximumBy compare
 
 myMinimum :: (Ord a) => [a] -> a
-myMinimum = undefined
+myMinimum = myMinimumBy compare
