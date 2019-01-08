119aed8f1205ee3f037b3a3f6028f558d3d906e9
Martín Valdés de León <m.valdesdeleon@sovanta.com>
2019-01-08 20:37:42 +0100

    Chapter 15, Monoid exercises.

--- ch15/exercises/src/Exercises.hs
+++ ch15/exercises/src/Exercises.hs
@@ -13,6 +13,8 @@ module Exercises
   , Comp
   , unComp
   , Validation
+  , Mem
+  , runMem
   ) where
 
 import           Test.QuickCheck hiding (Failure, Success)
@@ -48,6 +50,10 @@ data Trivial =
 instance Semigroup Trivial where
   _ <> _ = Trivial
 
+instance Monoid Trivial where
+  mempty = Trivial
+  mappend = (<>)
+
 instance Arbitrary Trivial where
   arbitrary = return Trivial
 
@@ -57,6 +63,10 @@ newtype Identity a =
 
 instance Semigroup a => Semigroup (Identity a) where
   (Identity a) <> (Identity b) = Identity (a <> b)
+
+instance Monoid a => Monoid (Identity a) where
+  mempty = Identity mempty
+  mappend = (<>)
 
 instance Arbitrary a => Arbitrary (Identity a) where
   arbitrary = Identity <$> arbitrary
@@ -68,6 +78,10 @@ data Two a b =
 
 instance (Semigroup a, Semigroup b) => Semigroup (Two a b) where
   (Two a b) <> (Two a' b') = Two (a <> a') (b <> b')
+
+instance (Monoid a, Monoid b) => Monoid (Two a b) where
+  mempty = Two mempty mempty
+  mappend = (<>)
 
 instance (Arbitrary a, Arbitrary b) => Arbitrary (Two a b) where
   arbitrary = do
@@ -122,6 +136,10 @@ instance Semigroup BoolConj where
   (BoolConj True) <> (BoolConj True) = BoolConj True
   _ <> _ = BoolConj False
 
+instance Monoid BoolConj where
+  mempty = BoolConj True
+  mappend = (<>)
+
 instance Arbitrary BoolConj where
   arbitrary = BoolConj <$> arbitrary
 
@@ -133,6 +151,10 @@ instance Semigroup BoolDisj where
   (BoolDisj True) <> (BoolDisj False) = BoolDisj True
   (BoolDisj False) <> (BoolDisj True) = BoolDisj True
   _ <> _ = BoolDisj False
+
+instance Monoid BoolDisj where
+  mempty = BoolDisj False
+  mappend = (<>)
 
 instance Arbitrary BoolDisj where
   arbitrary = BoolDisj <$> arbitrary
@@ -151,7 +173,7 @@ instance (Arbitrary a, Arbitrary b) => Arbitrary (Or a b) where
   arbitrary = oneof [Fst <$> arbitrary, Snd <$> arbitrary]
 
 newtype Combine a b = Combine
-  { unCombine :: (a -> b)
+  { unCombine :: a -> b
   }
 
 instance Show (Combine a b) where
@@ -160,11 +182,15 @@ instance Show (Combine a b) where
 instance Semigroup b => Semigroup (Combine a b) where
   (Combine f) <> (Combine g) = Combine (\a -> f a <> g a) -- We can avoid the lambda, since Semigroup b => Semigroup (a -> b)
 
+instance Monoid b => Monoid (Combine a b) where
+  mempty = Combine (const mempty)
+  mappend = (<>)
+
 instance (CoArbitrary a, Arbitrary b) => Arbitrary (Combine a b) where
   arbitrary = Combine <$> arbitrary
 
 newtype Comp a = Comp
-  { unComp :: (a -> a)
+  { unComp :: a -> a
   }
 
 instance Show (Comp a) where
@@ -172,6 +198,10 @@ instance Show (Comp a) where
 
 instance Semigroup (Comp a) where
   (Comp f) <> (Comp g) = Comp (f . g)
+
+instance Monoid (Comp a) where
+  mempty = Comp id
+  mappend = (<>)
 
 instance (CoArbitrary a, Arbitrary a) => Arbitrary (Comp a) where
   arbitrary = Comp <$> arbitrary
@@ -188,3 +218,24 @@ instance Semigroup a => Semigroup (Validation a b) where
 
 instance (Arbitrary a, Arbitrary b) => Arbitrary (Validation a b) where
   arbitrary = frequency [(1, Failure <$> arbitrary), (3, Success <$> arbitrary)]
+
+newtype Mem s a = Mem
+  { runMem :: s -> (a, s)
+  }
+
+instance Show (Mem s a) where
+  show (Mem _) = "Mem"
+
+instance Semigroup a => Semigroup (Mem s a) where
+  (Mem f) <> (Mem g) =
+    Mem $ \s ->
+      let (a', s') = g s
+          (a'', s'') = f s'
+       in (a'' <> a', s'')
+
+instance Monoid a => Monoid (Mem s a) where
+  mempty = Mem $ \s -> (mempty, s)
+  mappend = (<>)
+
+instance (CoArbitrary s, Arbitrary a, Arbitrary s) => Arbitrary (Mem s a) where
+  arbitrary = Mem <$> arbitrary
--- ch15/exercises/tests/tests.hs
+++ ch15/exercises/tests/tests.hs
@@ -5,23 +5,11 @@ import           Test.Hspec
 import           Test.QuickCheck
 
 import           Exercises       (BoolConj, BoolDisj, Combine, Comp, First',
-                                  Four, Identity, Or, Three, Trivial, Two,
-                                  Validation, unCombine, unComp)
+                                  Four, Identity, Mem, Or, Three, Trivial, Two,
+                                  Validation, runMem, unCombine, unComp)
 
 semigroupAssoc :: (Eq m, Semigroup m) => m -> m -> m -> Bool
 semigroupAssoc a b c = (a <> (b <> c)) == ((a <> b) <> c)
-
-combAssoc ::
-     (Eq b, Semigroup b)
-  => Combine a b
-  -> Combine a b
-  -> Combine a b
-  -> a
-  -> Bool
-combAssoc f g h a = unCombine (f <> (g <> h)) a == unCombine ((f <> g) <> h) a
-
-compAssoc :: Eq a => Comp a -> Comp a -> Comp a -> a -> Bool
-compAssoc f g h a = unComp (f <> (g <> h)) a == unComp ((f <> g) <> h) a
 
 monoidAssoc :: (Eq m, Monoid m) => m -> m -> m -> Bool
 monoidAssoc a b c = (a <> (b <> c)) == ((a <> b) <> c)
@@ -32,15 +20,61 @@ monoidLeftIdentity a = (mempty <> a) == a
 monoidRightIdentity :: (Eq m, Monoid m) => m -> Bool
 monoidRightIdentity a = (a <> mempty) == a
 
+combSemigroupAssoc ::
+     (Eq b, Semigroup b)
+  => Combine a b
+  -> Combine a b
+  -> Combine a b
+  -> a
+  -> Bool
+combSemigroupAssoc f g h a =
+  unCombine (f <> (g <> h)) a == unCombine ((f <> g) <> h) a
+
+combMonoidAssoc ::
+     (Eq b, Monoid b) => Combine a b -> Combine a b -> Combine a b -> a -> Bool
+combMonoidAssoc f g h a =
+  unCombine (f <> (g <> h)) a == unCombine ((f <> g) <> h) a
+
+combLeftIdentity :: (Eq b, Monoid b) => Combine a b -> a -> Bool
+combLeftIdentity f a = unCombine (mempty <> f) a == unCombine f a
+
+combRightIdentity :: (Eq b, Monoid b) => Combine a b -> a -> Bool
+combRightIdentity f a = unCombine (f <> mempty) a == unCombine f a
+
+compAssoc :: Eq a => Comp a -> Comp a -> Comp a -> a -> Bool
+compAssoc f g h a = unComp (f <> (g <> h)) a == unComp ((f <> g) <> h) a
+
+compLeftIdentity :: Eq a => Comp a -> a -> Bool
+compLeftIdentity f a = unComp (mempty <> f) a == unComp f a
+
+compRightIdentity :: Eq a => Comp a -> a -> Bool
+compRightIdentity f a = unComp (f <> mempty) a == unComp f a
+
+memMonoidAssoc ::
+     (Eq s, Eq a, Monoid a) => Mem s a -> Mem s a -> Mem s a -> s -> Bool
+memMonoidAssoc f g h a = runMem (f <> (g <> h)) a == runMem ((f <> g) <> h) a
+
+memLeftIdentity :: (Eq s, Eq a, Monoid a) => Mem s a -> s -> Bool
+memLeftIdentity f a = runMem (mempty <> f) a == runMem f a
+
+memRightIdentity :: (Eq s, Eq a, Monoid a) => Mem s a -> s -> Bool
+memRightIdentity f a = runMem (f <> mempty) a == runMem f a
+
 type FstAssoc = First' String -> First' String -> First' String -> Bool
 
 type FstId = First' String -> Bool
 
 type TrvAssoc = Trivial -> Trivial -> Trivial -> Bool
 
+type TrvId = Trivial -> Bool
+
 type IdAssoc a = Identity a -> Identity a -> Identity a -> Bool
 
+type IdId a = Identity a -> Bool
+
 type TwoAssoc a b = Two a b -> Two a b -> Two a b -> Bool
+
+type TwoId a b = Two a b -> Bool
 
 type ThreeAssoc a b c = Three a b c -> Three a b c -> Three a b c -> Bool
 
@@ -48,15 +82,27 @@ type FourAssoc a b c d = Four a b c d -> Four a b c d -> Four a b c d -> Bool
 
 type BoolConjAssoc = BoolConj -> BoolConj -> BoolConj -> Bool
 
+type BoolConjId = BoolConj -> Bool
+
 type BoolDisjAssoc = BoolDisj -> BoolDisj -> BoolDisj -> Bool
+
+type BoolDisjId = BoolDisj -> Bool
 
 type OrAssoc a b = Or a b -> Or a b -> Or a b -> Bool
 
 type CombAssoc a b = Combine a b -> Combine a b -> Combine a b -> a -> Bool
 
+type CombId a b = Combine a b -> a -> Bool
+
 type CompAssoc a = Comp a -> Comp a -> Comp a -> a -> Bool
 
+type CompId a = Comp a -> a -> Bool
+
 type ValAssoc a b = Validation a b -> Validation a b -> Validation a b -> Bool
+
+type MemAssoc s a = Mem s a -> Mem s a -> Mem s a -> s -> Bool
+
+type MemId s a = Mem s a -> s -> Bool
 
 main :: IO ()
 main =
@@ -67,10 +113,24 @@ main =
       it "Right identity" $ property (monoidRightIdentity :: FstId)
     describe "Trivial Semigroup" $ do
       it "Associative" $ property (semigroupAssoc :: TrvAssoc)
+    describe "Trivial Monoid" $ do
+      it "Associative" $ property (monoidAssoc :: TrvAssoc)
+      it "Left identity" $ property (monoidLeftIdentity :: TrvId)
+      it "Right identity" $ property (monoidRightIdentity :: TrvId)
     describe "Identity Semigroup" $ do
       it "Associative" $ property (semigroupAssoc :: IdAssoc String)
+    describe "Identity Monoid" $ do
+      it "Associative" $ property (monoidAssoc :: IdAssoc String)
+      it "Left identity" $ property (monoidLeftIdentity :: IdId String)
+      it "Right identity" $ property (monoidRightIdentity :: IdId String)
     describe "Two Semigroup" $ do
       it "Associative" $ property (semigroupAssoc :: TwoAssoc (Sum Int) String)
+    describe "Two Monoid" $ do
+      it "Associative" $ property (monoidAssoc :: TwoAssoc (Sum Int) String)
+      it "Left identity" $
+        property (monoidLeftIdentity :: TwoId (Sum Int) String)
+      it "Right identity" $
+        property (monoidRightIdentity :: TwoId (Sum Int) String)
     describe "Three Semigroup" $ do
       it "Associative" $
         property (semigroupAssoc :: ThreeAssoc (Sum Int) String [Bool])
@@ -80,13 +140,40 @@ main =
           (semigroupAssoc :: FourAssoc (Sum Int) String [Bool] (Product Int))
     describe "BoolConj Semigroup" $ do
       it "Associative" $ property (semigroupAssoc :: BoolConjAssoc)
+    describe "BoolConj Monoid" $ do
+      it "Associative" $ property (monoidAssoc :: BoolConjAssoc)
+      it "Left identity" $ property (monoidLeftIdentity :: BoolConjId)
+      it "Right identity" $ property (monoidRightIdentity :: BoolConjId)
     describe "BoolDisj Semigroup" $ do
       it "Associative" $ property (semigroupAssoc :: BoolDisjAssoc)
+    describe "BoolDisj Monoid" $ do
+      it "Associative" $ property (monoidAssoc :: BoolDisjAssoc)
+      it "Left identity" $ property (monoidLeftIdentity :: BoolDisjId)
+      it "Right identity" $ property (monoidRightIdentity :: BoolDisjId)
     describe "Or Semigroup" $ do
       it "Associative" $ property (semigroupAssoc :: OrAssoc (Sum Int) String)
     describe "Combine Semigroup" $ do
-      it "Associative" $ property (combAssoc :: CombAssoc Float (Product Int))
+      it "Associative" $
+        property (combSemigroupAssoc :: CombAssoc Float (Product Int))
+    describe "Combine Monoid" $ do
+      it "Associative" $
+        property (combMonoidAssoc :: CombAssoc Float (Product Int))
+      it "Left identity" $
+        property (combLeftIdentity :: CombId Float (Product Int))
+      it "Right identity" $
+        property (combRightIdentity :: CombId Float (Product Int))
     describe "Comp Semigroup" $ do
       it "Associative" $ property (compAssoc :: CompAssoc Float)
+    describe "Comp Monoid" $ do
+      it "Associative" $ property (compAssoc :: CompAssoc Float)
+      it "Left identity" $ property (compLeftIdentity :: CompId Float)
+      it "Right identity" $ property (compRightIdentity :: CompId Float)
     describe "Validation Semigroup" $ do
       it "Associative" $ property (semigroupAssoc :: ValAssoc (Sum Int) String)
+    describe "Mem Monoid" $ do
+      it "Associative" $
+        property (memMonoidAssoc :: MemAssoc Float (Product Int))
+      it "Left identity" $
+        property (memLeftIdentity :: MemId Float (Product Int))
+      it "Right identity" $
+        property (memRightIdentity :: MemId Float (Product Int))
