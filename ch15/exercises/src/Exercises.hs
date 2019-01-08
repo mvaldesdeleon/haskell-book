af1610864f941a3d053ba6ebee06d96ed2e59ae5
Martín Valdés de León <m.valdesdeleon@sovanta.com>
2019-01-08 01:25:09 +0100

    Chapter 15, Semigroup exercises.

--- ch15/exercises/src/Exercises.hs
+++ ch15/exercises/src/Exercises.hs
@@ -1,8 +1,21 @@
 module Exercises
   ( First'
+  , Trivial
+  , Identity
+  , Two
+  , Three
+  , Four
+  , BoolConj
+  , BoolDisj
+  , Or
+  , Combine
+  , unCombine
+  , Comp
+  , unComp
+  , Validation
   ) where
 
-import           Test.QuickCheck
+import           Test.QuickCheck hiding (Failure, Success)
 
 data Optional a
   = Nada
@@ -27,3 +40,151 @@ instance Arbitrary a => Arbitrary (Optional a) where
 
 instance Arbitrary a => Arbitrary (First' a) where
   arbitrary = First' <$> arbitrary
+
+data Trivial =
+  Trivial
+  deriving (Eq, Show)
+
+instance Semigroup Trivial where
+  _ <> _ = Trivial
+
+instance Arbitrary Trivial where
+  arbitrary = return Trivial
+
+newtype Identity a =
+  Identity a
+  deriving (Eq, Show)
+
+instance Semigroup a => Semigroup (Identity a) where
+  (Identity a) <> (Identity b) = Identity (a <> b)
+
+instance Arbitrary a => Arbitrary (Identity a) where
+  arbitrary = Identity <$> arbitrary
+
+data Two a b =
+  Two a
+      b
+  deriving (Eq, Show)
+
+instance (Semigroup a, Semigroup b) => Semigroup (Two a b) where
+  (Two a b) <> (Two a' b') = Two (a <> a') (b <> b')
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Two a b) where
+  arbitrary = do
+    a <- arbitrary
+    b <- arbitrary
+    return $ Two a b
+
+data Three a b c =
+  Three a
+        b
+        c
+  deriving (Eq, Show)
+
+instance (Semigroup a, Semigroup b, Semigroup c) =>
+         Semigroup (Three a b c) where
+  (Three a b c) <> (Three a' b' c') = Three (a <> a') (b <> b') (c <> c')
+
+instance (Arbitrary a, Arbitrary b, Arbitrary c) =>
+         Arbitrary (Three a b c) where
+  arbitrary = do
+    a <- arbitrary
+    b <- arbitrary
+    c <- arbitrary
+    return $ Three a b c
+
+data Four a b c d =
+  Four a
+       b
+       c
+       d
+  deriving (Eq, Show)
+
+instance (Semigroup a, Semigroup b, Semigroup c, Semigroup d) =>
+         Semigroup (Four a b c d) where
+  (Four a b c d) <> (Four a' b' c' d') =
+    Four (a <> a') (b <> b') (c <> c') (d <> d')
+
+instance (Arbitrary a, Arbitrary b, Arbitrary c, Arbitrary d) =>
+         Arbitrary (Four a b c d) where
+  arbitrary = do
+    a <- arbitrary
+    b <- arbitrary
+    c <- arbitrary
+    d <- arbitrary
+    return $ Four a b c d
+
+newtype BoolConj =
+  BoolConj Bool
+  deriving (Eq, Show)
+
+instance Semigroup BoolConj where
+  (BoolConj True) <> (BoolConj True) = BoolConj True
+  _ <> _ = BoolConj False
+
+instance Arbitrary BoolConj where
+  arbitrary = BoolConj <$> arbitrary
+
+newtype BoolDisj =
+  BoolDisj Bool
+  deriving (Eq, Show)
+
+instance Semigroup BoolDisj where
+  (BoolDisj True) <> (BoolDisj False) = BoolDisj True
+  (BoolDisj False) <> (BoolDisj True) = BoolDisj True
+  _ <> _ = BoolDisj False
+
+instance Arbitrary BoolDisj where
+  arbitrary = BoolDisj <$> arbitrary
+
+data Or a b
+  = Fst a
+  | Snd b
+  deriving (Eq, Show)
+
+instance Semigroup (Or a b) where
+  (Snd b) <> _ = Snd b
+  _ <> (Snd b) = Snd b
+  _ <> (Fst a) = Fst a
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Or a b) where
+  arbitrary = oneof [Fst <$> arbitrary, Snd <$> arbitrary]
+
+newtype Combine a b = Combine
+  { unCombine :: (a -> b)
+  }
+
+instance Show (Combine a b) where
+  show (Combine _) = "Combine"
+
+instance Semigroup b => Semigroup (Combine a b) where
+  (Combine f) <> (Combine g) = Combine (\a -> f a <> g a) -- We can avoid the lambda, since Semigroup b => Semigroup (a -> b)
+
+instance (CoArbitrary a, Arbitrary b) => Arbitrary (Combine a b) where
+  arbitrary = Combine <$> arbitrary
+
+newtype Comp a = Comp
+  { unComp :: (a -> a)
+  }
+
+instance Show (Comp a) where
+  show (Comp _) = "Comp"
+
+instance Semigroup (Comp a) where
+  (Comp f) <> (Comp g) = Comp (f . g)
+
+instance (CoArbitrary a, Arbitrary a) => Arbitrary (Comp a) where
+  arbitrary = Comp <$> arbitrary
+
+data Validation a b
+  = Failure a
+  | Success b
+  deriving (Eq, Show)
+
+instance Semigroup a => Semigroup (Validation a b) where
+  (Success b) <> _ = Success b
+  _ <> (Success b) = Success b
+  (Failure a) <> (Failure a') = Failure (a <> a')
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Validation a b) where
+  arbitrary = frequency [(1, Failure <$> arbitrary), (3, Success <$> arbitrary)]
--- ch15/exercises/tests/tests.hs
+++ ch15/exercises/tests/tests.hs
@@ -1,9 +1,27 @@
 module Main where
 
-import           Data.Monoid
+import           Data.Monoid     (Product, Sum)
+import           Test.Hspec
 import           Test.QuickCheck
 
-import           Exercises       (First')
+import           Exercises       (BoolConj, BoolDisj, Combine, Comp, First',
+                                  Four, Identity, Or, Three, Trivial, Two,
+                                  Validation, unCombine, unComp)
+
+semigroupAssoc :: (Eq m, Semigroup m) => m -> m -> m -> Bool
+semigroupAssoc a b c = (a <> (b <> c)) == ((a <> b) <> c)
+
+combAssoc ::
+     (Eq b, Semigroup b)
+  => Combine a b
+  -> Combine a b
+  -> Combine a b
+  -> a
+  -> Bool
+combAssoc f g h a = unCombine (f <> (g <> h)) a == unCombine ((f <> g) <> h) a
+
+compAssoc :: Eq a => Comp a -> Comp a -> Comp a -> a -> Bool
+compAssoc f g h a = unComp (f <> (g <> h)) a == unComp ((f <> g) <> h) a
 
 monoidAssoc :: (Eq m, Monoid m) => m -> m -> m -> Bool
 monoidAssoc a b c = (a <> (b <> c)) == ((a <> b) <> c)
@@ -14,12 +32,61 @@ monoidLeftIdentity a = (mempty <> a) == a
 monoidRightIdentity :: (Eq m, Monoid m) => m -> Bool
 monoidRightIdentity a = (a <> mempty) == a
 
-type FirstMappend = First' String -> First' String -> First' String -> Bool
+type FstAssoc = First' String -> First' String -> First' String -> Bool
 
 type FstId = First' String -> Bool
 
+type TrvAssoc = Trivial -> Trivial -> Trivial -> Bool
+
+type IdAssoc a = Identity a -> Identity a -> Identity a -> Bool
+
+type TwoAssoc a b = Two a b -> Two a b -> Two a b -> Bool
+
+type ThreeAssoc a b c = Three a b c -> Three a b c -> Three a b c -> Bool
+
+type FourAssoc a b c d = Four a b c d -> Four a b c d -> Four a b c d -> Bool
+
+type BoolConjAssoc = BoolConj -> BoolConj -> BoolConj -> Bool
+
+type BoolDisjAssoc = BoolDisj -> BoolDisj -> BoolDisj -> Bool
+
+type OrAssoc a b = Or a b -> Or a b -> Or a b -> Bool
+
+type CombAssoc a b = Combine a b -> Combine a b -> Combine a b -> a -> Bool
+
+type CompAssoc a = Comp a -> Comp a -> Comp a -> a -> Bool
+
+type ValAssoc a b = Validation a b -> Validation a b -> Validation a b -> Bool
+
 main :: IO ()
-main = do
-  quickCheck (monoidAssoc :: FirstMappend)
-  quickCheck (monoidLeftIdentity :: FstId)
-  quickCheck (monoidRightIdentity :: FstId)
+main =
+  hspec $ do
+    describe "First' Monoid" $ do
+      it "Associative" $ property (monoidAssoc :: FstAssoc)
+      it "Left identity" $ property (monoidLeftIdentity :: FstId)
+      it "Right identity" $ property (monoidRightIdentity :: FstId)
+    describe "Trivial Semigroup" $ do
+      it "Associative" $ property (semigroupAssoc :: TrvAssoc)
+    describe "Identity Semigroup" $ do
+      it "Associative" $ property (semigroupAssoc :: IdAssoc String)
+    describe "Two Semigroup" $ do
+      it "Associative" $ property (semigroupAssoc :: TwoAssoc (Sum Int) String)
+    describe "Three Semigroup" $ do
+      it "Associative" $
+        property (semigroupAssoc :: ThreeAssoc (Sum Int) String [Bool])
+    describe "Four Semigroup" $ do
+      it "Associative" $
+        property
+          (semigroupAssoc :: FourAssoc (Sum Int) String [Bool] (Product Int))
+    describe "BoolConj Semigroup" $ do
+      it "Associative" $ property (semigroupAssoc :: BoolConjAssoc)
+    describe "BoolDisj Semigroup" $ do
+      it "Associative" $ property (semigroupAssoc :: BoolDisjAssoc)
+    describe "Or Semigroup" $ do
+      it "Associative" $ property (semigroupAssoc :: OrAssoc (Sum Int) String)
+    describe "Combine Semigroup" $ do
+      it "Associative" $ property (combAssoc :: CombAssoc Float (Product Int))
+    describe "Comp Semigroup" $ do
+      it "Associative" $ property (compAssoc :: CompAssoc Float)
+    describe "Validation Semigroup" $ do
+      it "Associative" $ property (semigroupAssoc :: ValAssoc (Sum Int) String)
